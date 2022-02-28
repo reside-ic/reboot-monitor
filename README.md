98556d6692acc6434db209f3660857d2d71956bc
Wes Hinsley <w.hinsley@imperial.ac.uk>
2022-02-28 10:21:45 +0000

    Auto cert date detection

--- README.md
+++ README.md
@@ -1,24 +1,41 @@
 # reboot-monitor
 
-A very simple flask app which machines can regularly call with:-
+A very simple flask app which does some onitoring on when linux machines need rebooting
+due to automated security updates, and as a later addition, when SSL certificates are
+within 30 days of expiry. 
 
+## Reboot status
+
+For handling reboot status, put a file in `/etc/cron.daily` 
+on the server to be monitored, containing a modified version of:-
 ```
-https://monitor/?machine=Machine_Name&status=1
+#!/usr/bin/env bash
+if [ -f /var/run/reboot-required ]; then
+  curl "http://<monitor-address>/?machine=$hostname.dide.ic.ac.uk&status=1"
+else
+  curl "http://<monitor-address>/?machine=$hostname.dide.ic.ac.uk&status=0"
+fi
 ```
+(and `chmod` the file with `755` or course).
 
-where status 0 means the machine does not need a reboot, and any other status means that it does.
+The next day (or after manually triggering the script), check the `greylist.csv` 
+file for the hostname and IP address, and copy that line into `whitelist.csv`.
 
-Then having the server call `run.bat` in the `secret` folder, with that same folder as its 
-current path, will send a combined report of all the machines that have reported they need a reboot,
-to a teams channel. The report might say:-
 
-```
-Reboot Status
+## Certificate checks
 
-bill.company.com does not need rebooting, but did not report today.
-ben.company.com has needed a reboot since yesterday.
-weed.company.com has needed a reboot for 31 days!
-```
+Add a line to `certlist.csv` containing `server,port`.
+
+## The report
+
+Have the server call `run.bat` in the `secret` folder as a Windows Task, 
+with that same folder as its current path. This will send a combined
+report to a teams channel. 
+
+## Weirdness
 
 This is deployed as a flask app on `monitor.dide.ic.ac.uk` in the department, 
-using Xampp / Apache for Windows with mod_wsgi compiled in.
+using Xampp / Apache for Windows with mod_wsgi compiled in, which is a little
+eccentric, but shows it is possible to wrangle.
+
+```CSV files```
--- secret/report.py
+++ secret/report.py
@@ -1,7 +1,10 @@
 import csv, sys, requests, os
 from pathlib import Path
 from filelock import Timeout, FileLock
-from datetime import date
+from datetime import date, datetime, timedelta
+
+import OpenSSL
+import ssl, socket
 
 # This script sends a report to a teams webhook, if there are any machines
 # that reported they needed a reboot. Set the web hook below.
@@ -12,28 +15,53 @@ team_webhook = "<INSERT TEAM WEBHOOK HERE>"
 # tests, $file could be changed to "test", to avoid interfering with existing
 # data.
 
-file = "data";
+file = "data"
+cert = "certlist"
 
-os.chdir("C:/path/to/monitor/secret")
+# Bit hacky, but this is the path for the server on mrcdata.
+
+os.chdir("C:/xampp/FlaskApps/monitor/secret")
 
 ############################################################
 # If a file doesn"t exist, create and write the line to it #
 ############################################################
 
 def ensure_exists(fname, line):
-    if not Path(fname).exists():
+    if (not Path(fname).exists()):
         file = open(fname, "w")
         file.write(line + "\n")
         file.close()
+
+############################################################
+# Lookup a cert and report expiry or inaccessibility       #
+############################################################
+
+def test_cert_expiry(server, port, dt_now, dt_30):
+    try:
+        conn = ssl.create_connection((server, port))
+        context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+        sock = context.wrap_socket(conn, server_hostname=server)
+        cert = ssl.DER_cert_to_PEM_cert(sock.getpeercert(True))
+    except BaseException as err:
+        print(err)
+        return (type(err).__name__ + " reading cert for " + server + ":" + port+ "   \n")
+    x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
+    expiry = x509.get_notAfter().decode('ascii')[0:8]
+    expiry = datetime.strptime(expiry, "%Y%m%d")
+    if (dt_now > expiry):
+        return ("SSL cert " + server + " has expired!   \n")
+    if (dt_30 > expiry):
+        return ("SSL cert " + server + " expires on " + datetime.strftime(expiry, "%Y-%m-%d") + "   \n")
+    return ""
 
 ############################################################
 # Create pictorial indication of associated stress level   #
 ############################################################
 
 def emotional_payload(status):
-    if status > 400:
+    if (status > 400):
         return "&#x1F631;"
-    elif status > 100:
+    elif (status > 100):
         return "&#x1F62C;"
     else:
         return ""
@@ -42,6 +70,7 @@ def main():
     data_file = file + ".csv"
     data_lock_file = file + "lock"
     ensure_exists(data_lock_file, "lock")
+    cert_file = cert + ".csv"
 
     text = ""
     the_date = date.today().strftime("%Y-%m-%d")
@@ -54,29 +83,41 @@ def main():
             line_count = -1
             for row in reader:
                 line_count += 1
-                if line_count > 0:
+                if (line_count > 0):
                     machine = row[0]
                     status = int(row[1])
                     last_date = row[2]
                     date_info = ""
-                    if last_date != the_date:
+                    if (last_date != the_date):
                         date_info = ", but did not report today"
-                    if status == 0:
+                    if (status == 0):
                         text = text + machine + " does not need rebooting" + date_info + ".   \n"
-                    elif status == 1:
+                    elif (status == 1):
                         text = text + machine + " has needed a reboot since yesterday" + date_info + ".   \n"
-                    elif status > 1:
+                    elif (status > 1):
                          extra = emotional_payload(status)
-                         if last_date != the_date:
+                         if (last_date != the_date):
                              date_info = "It also did not report today."
-                         text = text + machine + " has need a reboot for **"
+                         text = text + machine + " has needed a reboot for **"
                          text = text + str(status) + " days!** " + extra + " " + date_info + "   \n"
+        
+        text = text + "   \n\n"
+        
+        dt_now = datetime.now()
+        dt_30 = dt_now + timedelta(days = 30)
+
+        with open(cert_file) as certfile:
+            reader = csv.reader(certfile, delimiter = ",")
+            line_count = -1
+            for row in reader:
+                line_count += 1
+                if (line_count > 0):
+                    text = text + test_cert_expiry(row[0], row[1], dt_now, dt_30)
 
     resp = requests.post(
         team_webhook,
-        json = dict(channel = "#bot-reboot", title = "Reboot Status", text = text),
+        json = dict(channel = "#bot-reboot", title = "Daily Update", text = text),
         verify = True)
-
 
 if __name__ == "__main__":
     main()
